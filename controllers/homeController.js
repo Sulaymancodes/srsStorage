37d54a97acba72df4e900e0ee1932c11c87b3582
sulaiman-rabiu <sulaymancodes@gmail.com>
2025-01-22 17:23:00 +0100

    Folder being created Successfully

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,18 +1,25 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const path = require("path");
+const fs = require("fs");
 
 async function getHome(req, res) {
     if (req.isAuthenticated()) {
         const userId = req.user.id;
-        const user = await prisma.user.findUnique({
-            where: {
-                id: userId,
-            },
-        })
-    
-        res.render("home", {username: `${user.username}`});
-    } else {
-        res.send("You are not authenticated");
+        try {
+            const user = await prisma.user.findUnique({
+                where: {
+                    id: userId,
+                },
+            })
+            const folders = await prisma.folder.findMany({
+              where: { userId: userId },
+            });
+            res.render("home", { username: `${user.username}`, folders });
+          } catch (err) {
+            console.error(err);
+            res.status(500).send("Error loading homepage");
+        }
     }
 }
 
@@ -25,4 +32,46 @@ function logOutUser (req, res) {
     })
 }
 
-module.exports = { getHome, logOutUser }
+async function createFolder (req, res) {
+    const userId = req.user.id;
+    const { folderName } = req.body;
+
+    try {
+        const newFolder = await prisma.folder.create({
+            data: {
+                name: folderName,
+                userId
+            }
+        })
+        const folderPath = path.join(__dirname, "../uploads", String(newFolder.id));
+        if (!fs.existsSync(folderPath)) {
+            fs.mkdirSync(folderPath, { recursive: true });
+        }
+
+        res.status(201).send("Folder created successfully!");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error creating folder");
+    } 
+}
+
+async function uploadFile (req, res) {
+    const { folderId } = req.body;
+    const filePath = req.file.path;
+
+    try {
+      await prisma.file.create({
+        data: {
+          name: req.file.filename,
+          path: filePath,
+          folderId: parseInt(folderId),
+        },
+      });
+      res.status(201).send("File uploaded successfully!");
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Error uploading file");
+    }
+}
+
+module.exports = { getHome, logOutUser, createFolder, uploadFile }
--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -3,21 +3,22 @@ const homeRouter = Router();
 const homeController = require('../controllers/homeController');
 const multer  = require('multer')
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, '/Users/hp/repos/uploads')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
-    }
-  })
+  destination: function (req, file, cb) {
+    const folderPath = path.join(__dirname, "../uploads", req.body.folderId);
+    cb(null, folderPath);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
   
 const upload = multer({ storage: storage })
 
 homeRouter.get("/home", homeController.getHome);
 homeRouter.get("/log-out", homeController.logOutUser);
-homeRouter.post('/uploadfile', upload.single('uploaded_file'), function (req, res) {
-   console.log(req.file.filename)
-});
+homeRouter.post("/create-folder", homeController.createFolder);
+homeRouter.post('/uploadfile', upload.single('uploaded_file'), homeController.uploadFile);
 
 module.exports = homeRouter;
