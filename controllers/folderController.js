601586cd1c4b7bae7d260a98dab32df41a7e3982
sulaiman-rabiu <sulaymancodes@gmail.com>
2025-01-23 15:17:36 +0100

    Folder Name and User Name is Being add to the Route

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,7 +1,28 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
 async function getFolder (req, res) {
-    const folderId = req.params.id;
-    
-    res.render("folder")
+    if(req.isAuthenticated()) {
+        const folderId = req.params.id;
+        const userId = req.user.id;
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId,
+            }
+        })
+        const folder = await prisma.folder.findUnique({
+            where: {
+                id: Number(folderId)
+            }
+        });
+        const folderName = folder.name;
+        const username = user.username;
+
+        res.render("folder", {username, folderName});
+    } else {
+        res.send("you are not authenticated to visit this page");
+    }
+  
 }
 
 
