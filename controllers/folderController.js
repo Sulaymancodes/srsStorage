c1b90de8059b4989b56abbb95a75d66e5db6e3f5
sulaiman-rabiu <sulaymancodes@gmail.com>
2025-01-26 21:14:37 +0100

    Uploaded Files can be Viewed in their Folders

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,40 +1,52 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const path = require("path");
-const fs = require("fs");
 
-async function getFolder (req, res) {
-    if(req.isAuthenticated()) {
-        const folderId = req.params.id;
-        const userId = req.user.id;
-        const user = await prisma.user.findUnique({
-            where: {
-                id: userId,
-            }
-        })
-        const folder = await prisma.folder.findUnique({
-            where: {
-                id: Number(folderId)
-            }
-        });
-        const folderName = folder.name;
-        const username = user.username;
 
-        res.render("folder", {username, folderName, folderId});
-    } else {
-        res.send("you are not authenticated to visit this page");
-    }
-  
+async function getFolder(req, res) {
+  if (req.isAuthenticated()) {
+      const folderId = req.params.id;
+      const userId = req.user.id;
+
+      try {
+          const user = await prisma.user.findUnique({
+              where: {
+                  id: userId,
+              },
+          });
+
+          const folder = await prisma.folder.findUnique({
+              where: {
+                  id: Number(folderId),
+              },
+          });
+
+          const files = await prisma.file.findMany({
+              where: {
+                  folderId: Number(folderId),
+              },
+          });
+
+          const folderName = folder.name;
+          const username = user.username;
+
+          res.render("folder", { username, folderName, folderId, files });
+      } catch (err) {
+          console.error(err);
+          res.status(500).send("Error retrieving folder or files");
+      }
+  } else {
+      res.send("You are not authenticated to visit this page");
+  }
 }
 
 async function uploadFile (req, res) {
-    const { folderId } = req.body;
+    const { folderId, fileName } = req.body;
     const filePath = req.file.path;
 
     try {
       await prisma.file.create({
         data: {
-          name: req.file.filename,
+          name: fileName,
           path: filePath,
           folderId: parseInt(folderId),
         },
